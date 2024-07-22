028c680b4d0f56a64db8ada6ca57af559e344581
Maxwel Barno <barxwells@gmail.com>
2024-07-22 12:45:31 +0300

    style: use PEP 8 style guide
    - enforce PEP 8 style guide using `black`

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,16 +4,17 @@ from ..config import Config
 
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
     db.init_app(app)
-    
+
     with app.app_context():
-        from . import routes, models
-        from .dashboard import dashboard_bp  # dashboard route 
-        app.register_blueprint(dashboard_bp, url_prefix='/dashboard') 
+        from .dashboard import dashboard_bp  # dashboard route
+
+        app.register_blueprint(dashboard_bp, url_prefix="/dashboard")
         db.create_all()
-        
+
     return app
